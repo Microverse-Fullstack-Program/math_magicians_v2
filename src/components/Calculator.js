5780e2c2efb12cc0a1ff3881dc0388cf10c13202
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-03-25 09:12:40 +0300

    Add event handler and state management to calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,80 +1,103 @@
-import React from 'react';
+import { React, useState } from 'react';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="calc-container">
-    <div className="output-bar" />
+const Calculator = () => {
+  const [calcObj, setCalculation] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-    <div>
-      <div className="button-rows">
-        <button type="button" name="AC">
-          AC
-        </button>
-        <button type="button" name="+/-">
-          +/-
-        </button>
-        <button type="button" name="%">
-          %
-        </button>
-        <button type="button" name="/">
-          ÷
-        </button>
-      </div>
-      <div className="button-rows">
-        <button type="button" name="7">
-          7
-        </button>
-        <button type="button" name="8">
-          8
-        </button>
-        <button type="button" name="9">
-          9
-        </button>
-        <button type="button" name="x">
-          X
-        </button>
+  const onClickEvent = (event) => {
+    const buttonName = event.currentTarget.textContent;
+    const newState = calculate(calcObj, buttonName);
+    setCalculation(newState);
+  };
+
+  const [next, operation, total] = calcObj;
+
+  return (
+    <div className="calc-container">
+      <div className="output-bar">
+        { total }
+        {' '}
+        { operation }
+        {' '}
+        { next }
       </div>
 
-      <div className="button-rows">
-        <button type="button" name="4">
-          4
-        </button>
-        <button type="button" name="5">
-          5
-        </button>
-        <button type="button" name="6">
-          6
-        </button>
-        <button type="button" name="-">
-          -
-        </button>
-      </div>
-      <div className="button-rows">
-        <button type="button" name="1">
-          1
-        </button>
-        <button type="button" name="2">
-          2
-        </button>
-        <button type="button" name="3">
-          3
-        </button>
-        <button type="button" name="+">
-          +
-        </button>
-      </div>
-      <div className="last-row">
-        <button type="button" name="0">
-          0
-        </button>
-        <button type="button" name=".">
-          .
-        </button>
-        <button type="button" name="=">
-          =
-        </button>
+      <div>
+        <div className="button-rows">
+          <button type="button" name="AC" onClick={onClickEvent}>
+            AC
+          </button>
+          <button type="button" name="+/-" onClick={onClickEvent}>
+            +/-
+          </button>
+          <button type="button" name="%" onClick={onClickEvent}>
+            %
+          </button>
+          <button type="button" name="/" onClick={onClickEvent}>
+            ÷
+          </button>
+        </div>
+        <div className="button-rows">
+          <button type="button" name="7" onClick={onClickEvent}>
+            7
+          </button>
+          <button type="button" name="8" onClick={onClickEvent}>
+            8
+          </button>
+          <button type="button" name="9" onClick={onClickEvent}>
+            9
+          </button>
+          <button type="button" name="x" onClick={onClickEvent}>
+            X
+          </button>
+        </div>
+
+        <div className="button-rows">
+          <button type="button" name="4" onClick={onClickEvent}>
+            4
+          </button>
+          <button type="button" name="5" onClick={onClickEvent}>
+            5
+          </button>
+          <button type="button" name="6" onClick={onClickEvent}>
+            6
+          </button>
+          <button type="button" name="-" onClick={onClickEvent}>
+            -
+          </button>
+        </div>
+        <div className="button-rows">
+          <button type="button" name="1" onClick={onClickEvent}>
+            1
+          </button>
+          <button type="button" name="2" onClick={onClickEvent}>
+            2
+          </button>
+          <button type="button" name="3" onClick={onClickEvent}>
+            3
+          </button>
+          <button type="button" name="+" onClick={onClickEvent}>
+            +
+          </button>
+        </div>
+        <div className="last-row">
+          <button type="button" name="0" onClick={onClickEvent}>
+            0
+          </button>
+          <button type="button" name="." onClick={onClickEvent}>
+            .
+          </button>
+          <button type="button" name="=" onClick={onClickEvent}>
+            =
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Calculator;
